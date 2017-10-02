8832d970572edb19af194e38c8bdfe874b1bbe85
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-02 17:40:08 +0200

    refactor(rpg game): doesn't work, but structured better

--- game_view.py
+++ game_view.py
@@ -2,43 +2,45 @@ from tkinter import *
 from entities import Hero
 root = Tk()
 
+class Map():
 
-canvas = Canvas(root, width='720', height='720')
+    def __init__(self):
+        self.canvas = Canvas(root, width='720', height='720')
 
-floor_tile = PhotoImage(file = "floor_tile.png")
-wall_tile = PhotoImage(file = "wall_tile.png")
+        self.floor_tile = PhotoImage(file = "floor_tile.png")
+        self.wall_tile = PhotoImage(file = "wall_tile.png")
 
-map_blueprint = [
-                [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-                [0, 0, 0, 1, 0, 1, 0, 1, 1, 0],
-                [0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
-                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-                [1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
-                [0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 1, 0, 1, 1, 0, 1, 0],
-                [0, 0, 0, 0, 0, 1, 1, 0, 1, 0],
-                [0, 1, 1, 1, 0, 0, 0, 0, 1, 0],
-                [0, 0, 0, 1, 0, 1, 1, 0, 1, 0]
-                                             ]
+        self.map_blueprint = [
+                    [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 1, 0, 1, 1, 0],
+                    [0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                    [1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+                    [0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
+                    [0, 1, 0, 1, 0, 1, 1, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 1, 1, 0, 1, 0],
+                    [0, 1, 1, 1, 0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 1, 0, 1, 1, 0, 1, 0]
+                                                ]
                 
-def draw_map():
-    x = 0
-    y = 0
+    def draw_map(self):
+        self.x = 0
+        self.y = 0
 
-    for row in range(len(map_blueprint)):
-        x = 0
-        for tile in range(len(map_blueprint[0])):
-            if map_blueprint[row][tile] == 1:
-                canvas.create_image(x, y, anchor=NW, image=wall_tile)
-            else:
-                canvas.create_image(x, y, anchor=NW, image=floor_tile)
-            x += 72
-        y += 72
+        for row in range(len(self.map_blueprint)):
+            self.x = 0
+            for tile in range(len(self.map_blueprint[0])):
+                if self.map_blueprint[row][tile] == 1:
+                    self.canvas.create_image(self.x, self.y, anchor=NW, image=self.wall_tile)
+                else:
+                    self.canvas.create_image(self.x, self.y, anchor=NW, image=self.floor_tile)
+                self.x += 72
+            self.y += 72
 
 
-
+mymap = Map()
 myhero = Hero(canvas)
-draw_map()
+mymap.draw_map()
 myhero.draw(0, 0)
 canvas.pack()
 root.mainloop()
