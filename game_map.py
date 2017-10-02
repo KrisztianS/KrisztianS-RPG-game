639812d4919c3b9a93e201aed5185e44286f41ac
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-02 15:42:34 +0200

    feat(rpg game): map with wall objects created

--- game_map.py
+++ game_map.py
@@ -3,19 +3,36 @@ root = Tk()
 
 canvas = Canvas(root, width='720', height='720')
 
-filename = PhotoImage(file = "floor_tile.png")
+floor_tile = PhotoImage(file = "floor_tile.png")
+wall_tile = PhotoImage(file = "wall_tile.png")
 
+map_blueprint = [
+                [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 1, 0, 1, 1, 0],
+                [0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [1, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+                [0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 1, 0, 1, 1, 0, 1, 0],
+                [0, 0, 0, 0, 0, 1, 1, 0, 1, 0],
+                [0, 1, 1, 1, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 1, 0, 1, 1, 0, 1, 0]
+                                             ]
+                
 def draw_map():
     x = 0
     y = 0
 
-    for row in range(10):
+    for row in range(len(map_blueprint)):
         x = 0
-        for tile in range(10):
-            canvas.create_image(x, y, anchor=NW, image=filename)
+        for tile in range(len(map_blueprint[0])):
+            if map_blueprint[row][tile] == 1:
+                canvas.create_image(x, y, anchor=NW, image=wall_tile)
+            else:
+                canvas.create_image(x, y, anchor=NW, image=floor_tile)
             x += 72
         y += 72
-        
+
 draw_map()
 canvas.pack()
 root.mainloop()
