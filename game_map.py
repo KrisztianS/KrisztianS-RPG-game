70328fbef32ddf23e28c13d21684955cb9fc75e0
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-02 14:37:24 +0200

    feat(rpg game): map created

--- game_map.py
+++ game_map.py
@@ -1,15 +1,21 @@
 from tkinter import *
 root = Tk()
 
-canvas = Canvas(root, width='600', height='600')
+canvas = Canvas(root, width='720', height='720')
 
 filename = PhotoImage(file = "floor_tile.png")
-image = canvas.create_image(0, 0, anchor=NW, image=filename)
 
+def draw_map():
+    x = 0
+    y = 0
+
+    for row in range(10):
+        x = 0
+        for tile in range(10):
+            canvas.create_image(x, y, anchor=NW, image=filename)
+            x += 72
+        y += 72
+        
+draw_map()
 canvas.pack()
-
-
-
-
-
 root.mainloop()
